5f214008932304375945c56f2e4cb9e7609df128
sijan67 <sijuichi1@gmail.com>
2023-11-05 21:44:26 -0800

    get alerts with its severity level

---
+++ test_api/stops_alert.py
@@ -0,0 +1,37 @@
+import requests
+import sys
+sys.path.append('..')
+
+from api_key import api
+from google.transit import gtfs_realtime_pb2
+
+api_key = api
+user_stop_id = "11302"  # Replace with the user-provided stop_id
+
+url = f"https://gtfs.translink.ca/v2/gtfsalerts?apikey={api_key}"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    feed = gtfs_realtime_pb2.FeedMessage()
+    feed.ParseFromString(response.content)
+    printed_stop_ids = set()  # To keep track of already printed stop_ids
+
+    for entity in feed.entity:
+        for informed_entity in entity.alert.informed_entity:
+            if informed_entity.stop_id == user_stop_id and user_stop_id not in printed_stop_ids:
+                # Print the desired information for the matching stop_id
+                print("URL:")
+                for translation in entity.alert.url.translation:
+                    print(translation.text)
+                print("Header Text:")
+                for translation in entity.alert.header_text.translation:
+                    print(translation.text)
+                print("Description Text:")
+                for translation in entity.alert.description_text.translation:
+                    print(translation.text)
+                print("Severity Level:", entity.alert.severity_level)
+                print()
+
+                # Add the stop_id to the printed_stop_ids set to avoid duplicate printing
+                printed_stop_ids.add(user_stop_id)
