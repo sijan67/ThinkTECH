3cc5f321f1251d023c9f31f95fbe26e2b676fff4
sijan67 <sijuichi1@gmail.com>
2023-11-06 13:21:37 -0800

    select different location from map and show address

--- frontend/CozyCommute/screens/MapPage/MapPage.js
+++ frontend/CozyCommute/screens/MapPage/MapPage.js
@@ -1,12 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image } from 'react-native';
-import MapView, { Marker , Callout} from 'react-native-maps';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import style from './style';
 import * as Location from 'expo-location';
+import config from '../../config/config';
+import Geocoder from 'react-native-geocoding';
 
 const Map = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [markerName, setMarkerName] = useState("Your Location"); // Initialize markerName with "Your Location"
+  const apiKey = config.googleMapsApiKey;
+  Geocoder.init(apiKey);
+ 
+
+  const getLocationName = async (latitude, longitude) => {
+    try {
+      const res = await Geocoder.from({ latitude, longitude });
+      const address = res.results[0].formatted_address;
+      
+      // Split the address string at the comma and take the first part
+        const addressname = address.split(',')[0].trim();
+
+        console.log("Location Name:", addressname);
+        setMarkerName(addressname);
+    } catch (error) {
+      console.error("Error getting location name:", error);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -30,61 +51,51 @@ const Map = () => {
     region = JSON.parse(text);
   }
 
+  const onMarkerDragEnd = (e) => {
+    // When the marker is dragged and dropped, update the markerName
+    console.log("dragged, " , e.nativeEvent.coordinate)
+    let latlon = e.nativeEvent.coordinate
+    getLocationName(latlon["latitude"], latlon["longitude"])
+
+  };
+
   return (
-    <View >
+    <View>
       {location && (
-        <MapView style={style.map} initialRegion={{
-          latitude: region.coords.latitude, //  current latitude
-          longitude: region.coords.longitude, //  current longitude
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}>
+        <MapView
+          style={style.map}
+          initialRegion={{
+            latitude: region.coords.latitude,
+            longitude: region.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
           <Marker
             coordinate={{
-              latitude: region.coords.latitude, //  current latitude
-              longitude: region.coords.longitude, //  current longitude
+              latitude: region.coords.latitude,
+              longitude: region.coords.longitude,
             }}
             image={require('../../assets/map_marker.png')}
-            title="Your Location"
-            description="You are here"
+            title={markerName} // Display the marker name
+            description="Your selected location"
             onSelect={() => console.log('onSelect')}
-            // onDrag={() => console.log('onDrag')}
             onDragStart={() => console.log('onDragStart')}
             draggable
+            onDragEnd={onMarkerDragEnd} // Call the onMarkerDragEnd function when marker is dropped
           />
-
-            {/* Show alerts + change location +  ( shelter information / input information + nearby stops ) */}
-            {/* <Callout tooltip>
-              <View>
-                <View style={style.bubble}>
-                  <Text style={style.name}>Favourite Restaurant</Text>
-                  <Text>A short description</Text>
-                  <Image 
-                    style={style.image}
-                    source={require('../../assets/bg.jpg')}
-                  />
-                </View>
-                <View style={style.arrowBorder} />
-                <View style={style.arrow} />
-              </View>
-            </Callout> */}
-        </MapView> 
+        </MapView>
       )}
       {location && (
         <View style={style.overlayView}>
           <View style={style.bubble}>
-          <View style={style.rowContainer}>
-          <View style={style.arrowBorder} />
-            <Text style={style.name}>99 bus</Text>
+            <View style={style.rowContainer}>
+              <View style={style.arrowBorder} />
+              <Text style={style.name}>99 bus</Text>
             </View>
             <Text>Short description</Text>
-            <Image
-              style={style.image}
-              source={require('../../assets/bg.jpg')}
-            />
+            <Image style={style.image} source={require('../../assets/bg.jpg')} />
           </View>
-          
-          {/* <View style={style.arrow} /> */}
         </View>
       )}
     </View>
