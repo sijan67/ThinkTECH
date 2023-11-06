be9eaaa863b8b63a429bf0915b30de2288805780
sijan67 <sijuichi1@gmail.com>
2023-11-06 10:27:04 -0800

    added callout on map

--- frontend/CozyCommute/screens/MapPage/MapPage.js
+++ frontend/CozyCommute/screens/MapPage/MapPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { View, Text, Image } from 'react-native';
+import MapView, { Marker , Callout} from 'react-native-maps';
 import style from './style';
 import * as Location from 'expo-location';
 
@@ -31,23 +31,39 @@ const Map = () => {
   }
 
   return (
-    <View style={style.container}>
-      <Text>Map Screen</Text>
+    <View >
       {location && (
         <MapView style={style.map} initialRegion={{
-          latitude: region.coords.latitude, // Your current latitude
-          longitude: region.coords.longitude, // Your current longitude
+          latitude: region.coords.latitude, //  current latitude
+          longitude: region.coords.longitude, //  current longitude
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}>
           <Marker
             coordinate={{
-              latitude: region.coords.latitude, // Your current latitude
-              longitude: region.coords.longitude, // Your current longitude
+              latitude: region.coords.latitude, //  current latitude
+              longitude: region.coords.longitude, //  current longitude
             }}
+            image={require('../../assets/map_marker.png')}
             title="Your Location"
             description="You are here"
           />
+
+            {/* Show alerts + change location +  ( shelter information / input information + nearby stops ) */}
+            <Callout tooltip>
+              <View>
+                <View style={style.bubble}>
+                  <Text style={style.name}>Favourite Restaurant</Text>
+                  {/* <Text>A short description</Text> */}
+                  <Image 
+                    style={style.image}
+                    source={require('../../assets/bg.jpg')}
+                  />
+                </View>
+                <View style={style.arrowBorder} />
+                <View style={style.arrow} />
+              </View>
+            </Callout>
         </MapView>
       )}
     </View>
