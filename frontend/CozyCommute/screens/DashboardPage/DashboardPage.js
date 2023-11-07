efaeb4f4e5cbac89eb875be0915d14fd71e0620b
sijan67 <sijuichi1@gmail.com>
2023-11-06 17:43:25 -0800

    simple get request integrated

--- frontend/CozyCommute/screens/DashboardPage/DashboardPage.js
+++ frontend/CozyCommute/screens/DashboardPage/DashboardPage.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import style from './style'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, TextInput } from 'react-native';
+import style from './style';
+import config from '../../config/config';
 
 function Dashboard() {
-return (
-<View style={style.container}>
-<Text>Dashboard Screen</Text>
-</View>
-)
+  const [response, setResponse] = useState(null);
+  const ipAddress = config.ip;
+
+  const handleGetRequest = () => {
+    fetch('http://'+ipAddress+':8000', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setResponse(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <View style={style.container}>
+      <Text>Dashboard Screen</Text>
+      <Button title="Get Data" onPress={handleGetRequest} />
+      <Text>Response from Server:</Text>
+      {response ? (
+        <Text>{JSON.stringify(response)}</Text>
+      ) : (
+        <Text>No response data yet.</Text>
+      )}
+    </View>
+  );
 }
-export default Dashboard
+
+export default Dashboard;
